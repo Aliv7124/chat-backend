c4ab75aea19015eac3eec80ea5684d9252c38b34
Aliv7124 <alivroy25@gmail.com>
2025-09-26 17:39:07 +0530

    Fix some  errorrrrs with k

--- socketio/server.js
+++ socketio/server.js
@@ -1,5 +1,5 @@
 
-/*
+
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
@@ -45,54 +45,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4002, () => console.log("Server running on 4002"));
-*/
-
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-
-const app = express();
-app.use(cors({
-  origin: "https://chat-z89o.vercel.app",
-  credentials: true,
-}));
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "https://chat-z89o.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-const users = {};
-
-io.on("connection", (socket) => {
-  const userId = socket.handshake.auth?.userId;
-  if (userId) {
-    users[userId] = socket.id;
-    console.log("User connected:", userId);
-  }
-
-  io.emit("getOnlineUsers", Object.keys(users));
-
-  socket.on("sendMessage", ({ conversationId, message, to }) => {
-    // If you have a single receiver userId
-    const receiverSocketId = users[to];
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("receiveMessage", message);
-    }
-    // Optionally emit to sender too for optimistic UI
-    socket.emit("receiveMessage", message);
-  });
-
-  socket.on("disconnect", () => {
-    if (userId) delete users[userId];
-    io.emit("getOnlineUsers", Object.keys(users));
-    console.log("User disconnected:", socket.id);
-  });
-});
 server.listen(4002, () => console.log("Server running on 4002"));
